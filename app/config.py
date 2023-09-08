6281f3c2624c20cc2da52a0c32728616d4d25022
JamesTheBard <jamesthebard@gmail.com>
2023-09-08 10:42:53 -0400

    Pull version info from pyproject.toml, bumped version.

--- app/config.py
+++ app/config.py
@@ -1,17 +1,24 @@
 import os
+import tomllib
 from datetime import datetime
+
+from box import Box
+
+with open('pyproject.toml', 'rb') as f:
+    pyproject = Box(tomllib.load(f))
 
 
 class Config:
     REDIS_URI = os.environ.get(
         "REDIS_URI", "redis://localhost:6379")
-    MONGO_URI = os.environ.get("MONGO_URI", "mongodb://root:root@localhost:27017")
+    MONGO_URI = os.environ.get(
+        "MONGO_URI", "mongodb://root:root@localhost:27017")
 
     CLIENT_EXIPRY = int(os.environ.get("CLIENT_EXPIRY", "10"))
     MONGO_DATA_DB = os.environ.get("MONGO_DATA_DB", "sisyphus_modules")
     MONGO_DATA_COLL_PREFIX = os.environ.get("MONGO_PREFIX", "data_")
 
-    VERSION = "0.9.0"
+    VERSION = pyproject.tool.poetry.version
     START_TIME = datetime.now()
 
     REDIS_QUEUE_PREFIX = os.environ.get('REDIS_QUEUE_PREFIX', 'queue')

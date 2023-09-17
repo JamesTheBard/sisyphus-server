253d5e99795b4d59175d289f89ab59fc3ce33ea6
JamesTheBard <jamesthebard@gmail.com>
2023-09-17 10:21:03 -0400

    Setting default logging level to INFO

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from datetime import datetime
 
 from pymongo import MongoClient
 import redis as r
+import logging
 from flask import Flask
 from flask_restx import Api, Resource
 
 from app.config import Config
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 api = Api(app, doc="/doc/")
 
 redis = r.from_url(Config.REDIS_URI)

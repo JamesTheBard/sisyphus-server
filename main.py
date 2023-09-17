2a70ba470c30837878404c204e1db3a99cf5af0c
JamesTheBard <jamesthebard@gmail.com>
2023-09-17 10:25:09 -0400

    Fixing logging under gunicorn

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from app import app
+import logging
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)

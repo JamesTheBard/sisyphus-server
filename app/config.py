9a7567a06f181559c0774b8b3dfe4214521aad68
JamesTheBard <jamesthebard@gmail.com>
2023-09-21 21:52:10 -0400

    Added new routes, models

--- app/config.py
+++ app/config.py
@@ -18,6 +18,11 @@ class Config:
     MONGO_DATA_DB = os.environ.get("MONGO_DATA_DB", "sisyphus_modules")
     MONGO_DATA_COLL_PREFIX = os.environ.get("MONGO_PREFIX", "data_")
 
+    MONGO_JOB_DB = os.environ.get("MONGO_JOB_DB", "sisyphus_jobs")
+    MONGO_JOB_FAILED = os.environ.get("MONGO_JOB_FAILED", "failed")
+    MONGO_JOB_COMPLETED = os.environ.get("MONGO_JOB_COMPLETED", "completed")
+    MONGO_JOB_QUEUED = os.environ.get("MONGO_JOB_QUEUED", "queued")
+
     VERSION = pyproject.tool.poetry.version
     START_TIME = datetime.now()
 
